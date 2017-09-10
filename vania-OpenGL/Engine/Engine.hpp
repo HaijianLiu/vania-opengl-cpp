b25b846485848378c926ab7405a3af5c18ee9ccd
HaijianLiu <haijanliu@gmail.com>
2017-09-10 11:49:16 +0900

    link opengl lib

---
+++ vania-OpenGL/Engine/Engine.hpp
@@ -0,0 +1,18 @@
+
+#ifndef Engine_hpp
+#define Engine_hpp
+
+// Include standard libraries
+#include <iostream>
+#include <random>
+
+// Include GLEW (include before gl.h and glfw.h)
+#include <GL/glew.h>
+// Include GLFW
+#include <GLFW/glfw3.h>
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#endif /* Engine_hpp */
--- vania-OpenGL/main.cpp
+++ vania-OpenGL/main.cpp
@@ -1,12 +1,5 @@
-//
-//  main.cpp
-//  vania-OpenGL
-//
-//  Created by haijian on 2017/09/10.
-//  Copyright © 2017年 haijian. All rights reserved.
-//
 
-#include <iostream>
+#include "Engine.hpp"
 
 int main(int argc, const char * argv[]) {
   // insert code here...
