5fd3332dfcccc1aeabadaf02798833cafcdecb2b
HaijianLiu <haijanliu@gmail.com>
2017-09-10 11:30:57 +0900

    gitignore

---
+++ vania-OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vania-OpenGL
+//
+//  Created by haijian on 2017/09/10.
+//  Copyright © 2017年 haijian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
