e0d76af6d4157e219f9c8002c5593c2827e63f80
HaijianLiu <haijanliu@gmail.com>
2017-09-12 23:02:57 +0900

    add: pixel scale to transform

--- vania-OpenGL/Engine/Transform.cpp
+++ vania-OpenGL/Engine/Transform.cpp
@@ -23,6 +23,6 @@ Transform::~Transform() {
 void Transform::update() {
 	this->model = glm::mat4();
 	this->model = glm::rotate(this->rotation.z,glm::vec3(0,0,1));
-	this->model = glm::scale(this->model, glm::vec3(this->scale.x,this->scale.y,this->scale.z));
+	this->model = glm::scale(this->model, glm::vec3(this->scale.x * PIXEL_SCALE, this->scale.y * PIXEL_SCALE, this->scale.z * PIXEL_SCALE));
 	this->model = glm::translate(this->model, glm::vec3(this->position.x,this->position.x,this->position.x));
 }
