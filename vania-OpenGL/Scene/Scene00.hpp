1ead7fc74e21f89c075bc9972a874897adb4c55e
HaijianLiu <haijanliu@gmail.com>
2017-09-18 12:52:20 +0900

    add: class Scene00 SceneManager

---
+++ vania-OpenGL/Scene/Scene00.hpp
@@ -0,0 +1,14 @@
+
+#ifndef Scene00_hpp
+#define Scene00_hpp
+
+class Scene00 : public Scene {
+private:
+	/* data */
+
+public:
+	Scene00();
+	~Scene00();
+};
+
+#endif /* Scene00_hpp */
---
+++ vania-OpenGL/Scene/SceneManager.hpp
@@ -0,0 +1,14 @@
+
+#ifndef SceneManager_hpp
+#define SceneManager_hpp
+
+class SceneManager {
+private:
+	/* data */
+
+public:
+	SceneManager();
+	~SceneManager();
+};
+
+#endif /* SceneManager_hpp */
